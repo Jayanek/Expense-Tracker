e58114385097edffbcecc72f9f0aca72a2f837f0
Jayanaka <jayanakawka@gmail.com>
2021-01-28 16:24:42 +0530

    add UIs for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import AddTransaction from './components/addTransaction';
+import Balanced from './components/balanced';
+import Header from './components/header';
+import History from './components/history';
+import IncomeExpense from './components/incomeExpense';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
+    <div>
+    <Header/>
+    <div className='container'>
+      <Balanced/>
+      <IncomeExpense/>
+      <History/>
+      <AddTransaction/>
+    </div>
     </div>
   );
 }
